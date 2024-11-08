631937140f040141db0de1aacc5e7dcd2a1f53dd
ShardulVanage <vsharadul@gmail.com>
2024-11-08 10:41:48 +0530

    map and images added

--- vite-project/src/pages/Gallery/components/MultiImg.jsx
+++ vite-project/src/pages/Gallery/components/MultiImg.jsx
@@ -48,11 +48,25 @@ const InViewImagesGrid = () => {
               "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876295/Gallery/pa3i8esjgvq48mknllcx.jpg",
               "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876295/Gallery/rrsb08xchyicddz6qshx.jpg",
               "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876294/Gallery/fmrpgxt6zw7kidnw9ccj.jpg",
-
               "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876294/Gallery/bdizrg7nkr31qzrm6j2b.jpg",
               "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876294/Gallery/hg4u0klfxydvar2mpgjo.jpg",
               "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876294/Gallery/alqwdo3c2hqw2fa0sywn.jpg",
               "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876294/Gallery/sgvnrkqmsykcv4xjr9np.jpg",
+
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986108/Gallery/vvt2im2kpsbbqjq6hhfg.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986107/Gallery/xdbkjcupuywr3efl1ik6.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986107/Gallery/nahmntwq44xrwqaycvny.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986106/Gallery/gbs9b1ryscomlmzepwpc.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986106/Gallery/wdkibqxo9bh6xm0rsyzy.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986106/Gallery/obloffhbagwrc8y5zbbl.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986106/Gallery/uhqkedvdfepausvn5vft.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986105/Gallery/z6gxyegvxfaz6s0ujg7x.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986104/Gallery/zasxw5lqhn8shefhczsp.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986104/Gallery/ofjqi0zff5yqhevjmr9o.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986104/Gallery/zb3jr69jkxkjf52js7n9.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986104/Gallery/cltkqv3tfbuconzlcjxe.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986104/Gallery/juxso5bb6rqpk1pijdzv.jpg",
+              "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986103/Gallery/kyp2onqcxpwbgwoano8y.jpg",
             ].map((imgSrc, index) => (
               <motion.div
                 key={index}
